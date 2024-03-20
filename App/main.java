34c05bcf176cc557d2082ca97bea11bd86588668
GuillaumeCarlin <g.carlin@eleve.leschartreux.net>
2024-03-20 17:19:51 +0100

    Ajout des déplacements d'un bouton

--- App/main.java
+++ App/main.java
@@ -4,22 +4,63 @@ package App;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
+import javafx.scene.input.KeyCode;
 
 
 public class main extends Application {
+    private Button bouton;
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Hello JavaFX");
-        btn.setOnAction(event -> System.out.println("Hello JavaFX"));
+        System.out.println("Start la page");
 
-        Scene scene = new Scene(btn, 300, 250);
+        bouton = new Button("Déplacement");
+        bouton.setPrefWidth(200);
+        bouton.setPrefHeight(50);
+        bouton.setLayoutX(100);
+        bouton.setLayoutY(100);
 
+        Pane root = new Pane();
+        root.getChildren().add(bouton);
+
+        Scene scene = new Scene(root, 1080, 720);
+
+
+        primaryStage.setResizable(false);
         primaryStage.setTitle("Hello JavaFX");
+
+        scene.setOnKeyReleased(event -> {
+            double deplacement = 10;
+            if (event.getCode() == KeyCode.Z) {
+                bouton.setLayoutY(bouton.getLayoutY() - deplacement);
+            } else if (event.getCode() == KeyCode.S) {
+                bouton.setLayoutY(bouton.getLayoutY() + deplacement);
+            } else if (event.getCode() == KeyCode.Q) {
+                bouton.setLayoutX(bouton.getLayoutX() - deplacement);
+            } else if (event.getCode() == KeyCode.D) {
+                bouton.setLayoutX(bouton.getLayoutX() + deplacement);
+            }
+        });
+
+        scene.setOnKeyPressed(event -> {
+            double deplacement = 10;
+            if (event.getCode() == KeyCode.Z) {
+                bouton.setLayoutY(bouton.getLayoutY() - deplacement);
+            } else if (event.getCode() == KeyCode.S) {
+                bouton.setLayoutY(bouton.getLayoutY() + deplacement);
+            } else if (event.getCode() == KeyCode.Q) {
+                bouton.setLayoutX(bouton.getLayoutX() - deplacement);
+            } else if (event.getCode() == KeyCode.D) {
+                bouton.setLayoutX(bouton.getLayoutX() + deplacement);
+            }
+        });
+
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+
 
     public static void main(String[] args) {
         launch(args);
--- concept/Pitch.md
+++ concept/Pitch.md
@@ -13,3 +13,7 @@ Montrez qu'il mobilise des techniques de POO avancée
 Comment allez-vous utiliser les patrons de conception ?
 Comment allez-vous utiliser l'architecture MVC ?
 
+On va utiliser l'architecture MVC en ayant :
+- un modèle qui va représenter les données de jeu comme les positions, les collisions, etc.
+- une vue qui va affiche la page de jeu
+- un contrôleur qui va gérer les interactions utilisateurs comme les commandes. Le contrôleur va mettre à jour le modèle et actualiser la vue pour implémenter ses nouvelles données.
