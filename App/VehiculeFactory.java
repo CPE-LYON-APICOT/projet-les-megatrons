6c045e64fdb47015687543ed822c032183607aa4
GuillaumeCarlin <g.carlin@eleve.leschartreux.net>
2024-03-27 16:08:45 +0100

    Ajout de la trainée

---
+++ App/VehiculeFactory.java
@@ -0,0 +1,18 @@
+package App;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class VehiculeFactory {
+    public VehiculeFactory() {
+    }
+
+    public Vehicule CreateVehicule(VehiculeType vehicule) {
+        return switch (vehicule) {
+            case VehiculeType.MOTO -> new Moto(100, 100, 1, 1, 50, new Rectangle(50, 10, Color.web("#2ddff3")), 10.0);
+            case VehiculeType.VOITURE -> null;
+            case CAMION -> null;
+            default -> null;
+        };
+    }
+}
